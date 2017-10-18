5c2a1f4f59b6c5b4b811e7dac0e48237dfdf0b67
tanat1994 <tanat1994@gmail.com>
2017-10-18 13:46:00 +0700

    Laravel Shopping Payment video#11

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -12,10 +12,32 @@
             <h1>Checkout</h1>
             <h4>Your Total: ${{ $total }}</h4>
                 <form action="{{ route('checkout') }}" method="post" id="checkout-form">
+                        <form action="/charge" method="post" id="payment-form">
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <label for="card-number">Card Number</label>
+                                    <input type="text" id="card-number" class="form-control" requied>
+                                </div>
 
-                
+                                <div class="col-xs-12">
+                                    <label for="card-cvc">Card CVC</label>
+                                    <input type="text" id="card-cvc" class="form-control" requied>
+                                </div>
+
+                                <div class="col-xs-12">
+                                    <label for="card-name">Card Holder Name</label>
+                                    <input type="text" id="card-name" class="form-control" requied>
+                                </div>
+
+                            </div>
+                            <button>Submit Payment</button>
+                        </form>
                 </form>
         </div>
     </div>
+@endsection
 
+@section('scripts')
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="{{ URL::to('js/credit.js') }}"></script>
 @endsection
